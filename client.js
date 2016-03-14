72ee6bcb79ea9760c15eb991057e9ceb79a82da7
Yoshua Wuyts <i@yoshuawuyts.com>
2016-03-15 08:02:42 +1100

    elements/section: add data

--- client.js
+++ client.js
@@ -1,11 +1,44 @@
 const sf = require('sheetify')
 
 const sidebar = require('./elements/sidebar')
+const content = require('./elements/content')
+const section = require('./elements/section')
 const layout = require('./layouts/main')
 
 sf('css-wipe')
 
-const tree = layout(sidebar, content)
-document.body.appendChild(tree)
+const data = [
+  [
+    'work', [
+      [ '2012 ▪ 2014', 'Fabrique', 'Office manager' ],
+      [ '2014 ▪ 2015', 'Wercker', 'Engineer' ],
+      [ '2015 ▪ 2016', 'Freelance', 'Creative Developer' ]
+    ]
+  ],
+  [
+    'communication skills',
+    [ 'Public speech', 'Debate & discussion', 'Copywriting' ]
+  ],
+  [
+    'off work',
+    [ '2012 ▪ 2013', 'VSPVU', 'Student union board member' ],
+    [ '2014 ▪ 2016', 'Nodeschool', 'Mentor' ]
+  ],
+  [
+    'interests',
+    [ 'Making things, moving & travel' ]
+  ],
+  [
+    'education',
+    [ '2004 ▪ 2011', 'Augustinianum', 'VWO - economics & social studies' ],
+    [ '2011 ▪ 2015', 'Vrije Universiteit Amsterdam', ' Psychology bachelor' ],
+    [ '2013 ▪ 2014', 'University of Amsterdam', 'Programming minor' ]
+  ],
+  [
+    'programming skills',
+    [ 'Writing modules ★★★★★' ]
+  ]
+]
 
-function content () {}
+const tree = layout(sidebar, content(section, data))
+document.body.appendChild(tree)
--- elements/content.js
+++ elements/content.js
@@ -1,13 +1,18 @@
 const sf = require('sheetify')
 const yo = require('yo-yo')
 
-const prefix = sf``
+const prefix = sf`.hi{}`
 
 module.exports = content
 
-function content () {
-  return yo`
-    <section class=${prefix}>
-    </section>
-  `
+function content (section, data) {
+  return function () {
+    return yo`
+      <section class=${prefix}>
+        ${data.map(function (item) {
+          return section(item[0], item[1])
+        })}
+      </section>
+    `
+  }
 }
